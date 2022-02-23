03e60ea74d087f665f0dfa42efa794e52c96fe91
Sebastian Guevara <sebastianguevara32569830@gmail.com>
2022-02-23 18:51:28 -0300

    finalizado el header

--- src/Logo.js
+++ src/Logo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "./images/Vector.png";
+import carrito from "./images/carrito.png";
+import login from "./images/login.png";
 
 const Logo = () => {
   return (
@@ -8,6 +10,17 @@ const Logo = () => {
       <p>
         Central<span>Music</span>
       </p>
+      <nav>
+        <a>Guitars</a>
+        <a>Accesories</a>
+        <a>Storage</a>
+        <a>Lessons</a>
+        <a>Repairs</a>
+      </nav>
+      <div>
+        <img src={carrito} alt="carrito"></img>
+        <img src={login} alt="login"></img>
+      </div>
     </div>
   );
 };
